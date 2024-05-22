7c65d78f3fb3a6d3a0bdda9001aa185670c3667c
Mohamed <mohakatalonya10@gmail.com>
2024-05-22 14:27:32 +0200

    fixed career section text size on mobile

--- src/CareerCard.js
+++ src/CareerCard.js
@@ -4,7 +4,7 @@ import { FaExternalLinkAlt } from 'react-icons/fa';
 const CareerCard = ({ info }) => {
   return (
     <div
-      className="career-card font-[600] sm:font-[700] text-[1rem] sm:text-[1.25rem] lg:text-[1.5rem] md:h-[350px] text-justify flex flex-col relative overflow-hidden outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-offset-2 shadow-md rounded-lg motion-reduce:transition-none"
+      className="career-card font-[600] text-[12.8px] sm:text-[1rem] md:text-[1.25rem] lg:text-[1.5rem] sm:font-[700] md:h-[350px] text-justify flex flex-col relative overflow-hidden outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-offset-2 shadow-md rounded-lg motion-reduce:transition-none"
       tabIndex="-1"
     >
       <div className="p-3 z-10 w-full justify-start items-center shrink-0 overflow-inherit color-inherit subpixel-antialiased rounded-t-lg flex gap-3">
@@ -20,17 +20,17 @@ const CareerCard = ({ info }) => {
           />
         </div>
         <div className="flex flex-col">
-          <p className="text-[1rem] sm:text[1.25rem] md:text-[1.5rem] lg:text-[2rem] text-[#11071F] font-[700]">
+          <p className="!text-base !sm:text[1.25rem] !md:text-[1.5rem] !lg:text-[2rem] text-[#11071F] font-[700]">
             {info.schoolName}
           </p>
-          <p className="text-base text-black font-[700]">
+          <p className="!text-base text-black font-[700]">
             {info.schoolDescription}
           </p>
         </div>
       </div>
       <hr className="border-clr-5" />
       <div className="relative w-full p-3 flex-auto flex-col align-items-inherit h-auto break-words text-left overflow-y-auto subpixel-antialiased flex justify-center">
-        <p className="text-[12.8px] md:text-[1rem] career-bio-text text-justify">
+        <p className="career-bio-text text-justify">
           {info.bio}
         </p>
       </div>
